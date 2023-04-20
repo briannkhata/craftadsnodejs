4e0947ad2624d6455819c5ac05b4a35aad70ca55
briannkhata <briannkhata@gmail.com>
2023-04-20 10:50:32 +0200

    hkeeping

--- Routes/Category.js
+++ Routes/Category.js
@@ -6,27 +6,27 @@ const Category = Router();
 
 Category.get(
   "/getALl/",
-  passport.authenticate("jwt", { session: false }),
+  //passport.authenticate("jwt", { session: false }),
   controller.GetAll
 );
 Category.get(
   "/getById/:id",
-  passport.authenticate("jwt", { session: false }),
+  // passport.authenticate("jwt", { session: false }),
   controller.GetById
 );
 Category.post(
   "/add/",
-  passport.authenticate("jwt", { session: false }),
+  //passport.authenticate("jwt", { session: false }),
   controller.Add
 );
 Category.put(
   "/delete/:id",
-  passport.authenticate("jwt", { session: false }),
+  // passport.authenticate("jwt", { session: false }),
   controller.Remove
 );
 Category.put(
   "/update/",
-  passport.authenticate("jwt", { session: false }),
+  // passport.authenticate("jwt", { session: false }),
   controller.Update
 );
 
